6aea153ed661f7893daeeea45a052d554e210f02
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-01-26 23:36:38 -0500

    Update glassdoorv2.py

--- glassdoorv2.py
+++ glassdoorv2.py
@@ -169,4 +169,3 @@ if __name__ == "__main__":
                                                 else:
                                                         print("Your search for %s, in %s does not match any jobs"%(keyword,place))
 #print(scraped_data.get("Url"))
-
